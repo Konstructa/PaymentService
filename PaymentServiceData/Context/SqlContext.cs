b8e014d43b6c750ef6f08b79277f476dcff881c2
Milena Elen <elenprojects7@gmail.com>
2022-12-02 11:58:43 -0300

    FEAT: create Context

---
+++ PaymentServiceData/Context/SqlContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentServiceBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PaymentServiceData.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Key> Transactions { get; set; }
+        public DbSet<Payer> Payers { get; set; }
+        public DbSet<Receiver> Receivers { get; set; }
+        public DbSet<Key> Keys { get; set; }
+    }
+}
