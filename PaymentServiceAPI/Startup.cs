ec92410bcf3bc238f01bb3ec332e063c28e57ad2
Milena Elen <elenprojects7@gmail.com>
2022-12-02 14:13:42 -0300

    FEAT: add dbcontext

--- PaymentServiceAPI/Startup.cs
+++ PaymentServiceAPI/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PaymentServiceData.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,7 @@ namespace PaymentServiceAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<SqlContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Main")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
